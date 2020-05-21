18e16905d229e52e3d57481c99a8500499851829
Adam <adammonast@Adams-MBP.attlocal.net>
2020-05-21 15:54:12 -0400

    MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,10 +1,8 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-# TO-DO: complete the helper function below to merge 2 sorted arrays
 
 
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
-
     # making an array of 0s that is the length of the integer
     merged_arr = [0] * elements
 
@@ -26,7 +24,7 @@ def merge(arrA, arrB):
             b += 1
 
         i += 1
-    # while individual arrs are greater than index
+    # while individual arrs are greater than indexes
     while a < len(arrA):
         merged_arr[i] = arrA[a]
         a += 1
@@ -69,11 +67,50 @@ def merge_sort(arr):
 def merge_in_place(arr, start, mid, end):
     # Your code here
 
+    start2 = mid + 1
+
+    # If the direct merge is already sorted
+    if (arr[mid] <= arr[start2]):
+        return
+
+    # Two pointers to maintain start
+    # of both arrays to merge
+    while (start <= mid and start2 <= end):
+
+        # If element 1 is in right place
+        if (arr[start] <= arr[start2]):
+            start += 1
+        else:
+            value = arr[start2]
+            index = start2
+
+            # Shift all the elements between element 1
+            # element 2, right by 1.
+            while (index != start):
+                arr[index] = arr[index - 1]
+                index -= 1
+
+            arr[start] = value
+
+            # Update all the pointers
+            start += 1
+            mid += 1
+            start2 += 1
+
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+
+    if l < r:
+        # Same as (l + r) / 2, but avoids overflow
+        # for large l and r
+        m = l + (r - l) // 2
+        # Sort first and second halves
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m + 1, r)
+        merge_in_place(arr, l, m, r)
 
     return arr
 
